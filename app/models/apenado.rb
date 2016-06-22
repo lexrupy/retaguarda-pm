3d4ae08fa03710f50a15ae0460054eed5fd4af88
Alexandre da Silva <lexrupy@gmail.com>
2016-06-22 14:28:21 -0300

    melhoras imagem

--- app/models/apenado.rb
+++ app/models/apenado.rb
@@ -1,4 +1,4 @@
 class Apenado < ActiveRecord::Base
-  has_many :photos
+  has_many :photos, dependent: :destroy
   accepts_nested_attributes_for :photos, allow_destroy: true
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,12 +5,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   process convert: 'jpg'
-  process resize_to_fill(200,200)
+  process resize_to_fit(720,720)
 
   #process :tags => ['post_picture']
   
   version :thumbnail do
-    resize_to_fit(100, 100)
+    resize_to_fit(120, 120)
   end
 
 end
