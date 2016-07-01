536cb404a774c5e81b50773964546d10029f6d64
Alexandre da Silva <lexrupy@gmail.com>
2016-07-01 00:53:24 -0300

    fix exibicao programas do dia atual

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
   def show
-    @programas = Programa.where("data_inicio < ? and data_fim > ?", Date.today, Date.today)
+    @programas = Programa.where("data_inicio <= ? and data_fim > ?", Date.today, Date.today)
   end
 end
