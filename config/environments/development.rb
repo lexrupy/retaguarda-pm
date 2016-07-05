0e524bcb3a5c4acfc0d1864793c560930887d6ae
Alexandre da Silva <lexrupy@gmail.com>
2016-07-04 23:17:40 -0300

    timezone

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
 
   config.i18n.default_locale = 'pt-BR'
 
+  config.time_zone = 'Brasilia'
+  config.active_record.default_timezone = 'Brasilia'
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
